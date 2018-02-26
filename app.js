3bcc4eaa1edd4ecd91a716cd1b0a487965a321ab
harrzhou1103 <yzhou108@dons.usfca.edu>
2018-02-25 18:59:05 -0800

    Added cloud-employed code

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 8080, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
 });
